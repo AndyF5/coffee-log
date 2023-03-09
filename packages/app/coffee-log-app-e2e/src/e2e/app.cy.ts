922dc3e111be61276a2d43f04b73b08c39cb00a1
Andrew Faulkner <afaulkner@neurosolutionsgroup.com>
2023-03-08 19:52:23 -0500

    feat: 1 - Add Firebase config

--- packages/app/coffee-log-app-e2e/src/e2e/app.cy.ts
+++ packages/app/coffee-log-app-e2e/src/e2e/app.cy.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('app-coffee-log-app', () => {
+describe('coffee-log-app', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
--- packages/app/coffee-log-app/jest.config.ts
+++ packages/app/coffee-log-app/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'app-coffee-log-app',
+  displayName: 'coffee-log-app',
   preset: '../../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
